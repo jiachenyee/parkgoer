30c6a155f3d657c1b03e9ea52211ca2e57a24bb9
Jia Chen <github@jiachen.app>
2025-07-03 17:37:15 +0800

    Initial Commit

---
+++ Parkgoer/ParkgoerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkgoerApp.swift
+//  Parkgoer
+//
+//  Created by Jia Chen Yee on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ParkgoerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
